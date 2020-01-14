abefd6b6efaf397d3e7fd8e471a959aeca85e162
picongzhi <picongzhi@gmail.com>
2020-01-15 01:28:19 +0800

    feat(user): add user register

--- app/web/auth.py
+++ app/web/auth.py
@@ -1,9 +1,23 @@
+from flask import render_template, request
+
 from . import web
+from app.forms.auth import RegisterForm
+from app.models.user import User
+from app.models.base import db
 
 
 @web.route('/register', methods=['GET', 'POST'])
 def register():
-    pass
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User()
+        user.set_attrs(form.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+    return render_template('auth/register.html',
+                           form=form)
 
 
 @web.route('/login', methods=['GET', 'POST'])
